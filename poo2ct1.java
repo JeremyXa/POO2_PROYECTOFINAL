539d5ddc19d42acbc04de15daa378740ce16c958
kirby107 <estebanchariarse@gmail.com>
2025-10-26 13:02:34 -0500

    Update poo2ct1.java

--- poo2ct1.java
+++ poo2ct1.java
@@ -9,9 +9,8 @@ package com.mycompany.mavenproject1;
  * @author USUARIO
  */
 public class Mavenproject1 {
-xczxczcz
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
-aea ok
